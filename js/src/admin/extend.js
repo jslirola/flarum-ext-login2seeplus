a8deac3d7c9b77a59510e2b22b6f44105dc2a20a
Javier S. Lirola <6697559+jslirola@users.noreply.github.com>
2026-09-02 21:13:14 +0200

    Fix link/image hiding messages reported by ClaudiusH
    - An <a> whose sole content is an <img> (e.g. fof/photoswipe's
      lightbox markup) now gets the image message instead of the
      generic link message.
    - Expose the existing 'hide destination, keep link text' mode
      as a proper admin select option instead of a dead, unreachable
      branch behind a boolean switch.

--- js/src/admin/extend.js
+++ js/src/admin/extend.js
@@ -12,8 +12,14 @@ export default [
         .customSetting(() => <legend className="login2seeplus-hide">{app.translator.trans('jslirola-login2seeplus.admin.hide')}</legend>)
         .setting(() => ({
             setting: 'jslirola.login2seeplus.link',
-            type: 'switch',
-            label: app.translator.trans('jslirola-login2seeplus.admin.link')
+            type: 'select',
+            label: app.translator.trans('jslirola-login2seeplus.admin.link'),
+            options: {
+                '0': app.translator.trans('jslirola-login2seeplus.admin.link_off'),
+                '1': app.translator.trans('jslirola-login2seeplus.admin.link_hide'),
+                '2': app.translator.trans('jslirola-login2seeplus.admin.link_mask'),
+            },
+            default: '1'
         }))
         .setting(() => ({
             setting: 'jslirola.login2seeplus.image',
